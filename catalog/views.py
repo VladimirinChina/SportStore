deb33427fc43675c0c27299e90169a5e08f96fb4
vladimir <vladimirinchina@gmail.com>
2026-08-15 13:24:48 +0800

    Add product views and templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,26 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 
 def home(request: HttpRequest) -> HttpResponse:
     """Отображает главную страницу магазина."""
 
-    return render(request, "catalog/home.html")
+    products = Product.objects.all()
+
+    return render(request, "catalog/home.html", {"products": products})
 
 
 def contacts(request: HttpRequest) -> HttpResponse:
     """Отображает страницу контактов."""
 
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    """Отображает страницу с подробной информацией о товаре."""
+
+    product = Product.objects.get(pk=pk)
+
+    return render(request, "catalog/product_detail.html", {"product": product})
